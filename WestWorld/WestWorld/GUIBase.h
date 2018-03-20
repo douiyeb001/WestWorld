27a62ae4f1a1d410eac70ec8600797e25d5b72fe
YaBoyPete <peter.marcelis@hva.nl>
2018-03-20 15:26:45 +0100

    Added basic method to add HUD added Healthbar and Placeable class (to be extended)

---
+++ WestWorld/WestWorld/GUIBase.h
@@ -0,0 +1,28 @@
+#ifndef GUIBASE_H
+#define GUIBASE_H
+
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace core;
+using namespace gui;
+using namespace video;
+using namespace std;
+
+class HealthBar {
+public:
+	HealthBar(IVideoDriver* driver, char const* bar);
+	void Draw(IVideoDriver* driver);
+private:
+	ITexture* hpBar;
+};
+
+class Placeable {
+public:
+	Placeable(IVideoDriver* driver, char const* icon);
+	void Draw(IVideoDriver* driver);
+private:
+	ITexture * placeableIcon;
+};
+
+#endif
