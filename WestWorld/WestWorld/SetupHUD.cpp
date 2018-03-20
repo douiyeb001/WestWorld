df5c54b4bf5e4d6285ccf7d3beca4157287f7811
YaBoyPete <peter.marcelis@hva.nl>
2018-03-20 15:31:52 +0100

    Added GUIBaseclass and a HUD setup

---
+++ WestWorld/WestWorld/SetupHUD.cpp
@@ -0,0 +1,18 @@
+#include "SetupHUD.h"
+
+SetupHUD::SetupHUD(){
+
+}
+
+
+
+void SetupHUD::createHUD(IVideoDriver* driver) {
+	hp = new HealthBar(driver, "media/UI/HealthBarDefinitelyNotStolen.png");
+	hp->Draw(driver);
+	icon1 = new Placeable(driver, "media/UI/towerIcon.png");
+	icon2 = new Placeable(driver, "media/UI/trapIcon.png");
+	icon3 = new Placeable(driver, "media/UI/cannonIcon.png");
+	icon1->Draw(driver);
+	icon2->Draw(driver);
+	icon3->Draw(driver);
+}
