d449161a0b1536b7664ce034fd73be2e79ef0b79
Unknown <bilal.douiyeb@hva.nl>
2018-04-23 14:12:55 +0200

    [EDIT]Game manager has been updated

--- WestWorld/WestWorld/EnemyManager.cpp
+++ WestWorld/WestWorld/EnemyManager.cpp
@@ -11,45 +11,23 @@ EnemyManager::EnemyManager(scene::ISceneManager* smgr, scene::ITriangleSelector*
 	idriver = driver;
 }
 
-void EnemyManager::CreateEnemy() {
-	Enemy * test1 = new Enemy();
-	test1->Init(ismgr, iselector, imeta, idriver);
-	//enemyList.push_back(test1);
-	enemyList.push_back(test1);
-}
 
-void EnemyManager::FillList(Enemy* enemy){
-	// add enemy to list
-	//enemyList.push_back(enemy);
-	// GET ALL ENEMIES FROM SCENE OLD ////////////////////////////////////////////// REE
-	//core::array<scene::ISceneNode *> nodes;
-	//smgr->getSceneNodesFromType(scene::ESNT_ANY, nodes); // Find all nodes
 
-	//for (u32 i = 0; i < nodes.size(); ++i)
-	//{
-	//	scene::ISceneNode * node = nodes[i];
+void EnemyManager::FillList(Opponent* enemy){
 
-	//	switch (node->getID())
-	//	{
-	//	case 17:
-	//		enemyList.push_back(node);
-	//		break;
-	//	}
-	//}
+	opponentList.push_back(enemy);
+
+
 }
 
 void EnemyManager::CheckCollision(scene::ISceneNode *hitObject) {
 	imeta->addTriangleSelector(iselector);
-	bool koekje = false;
-	for (Enemy* item : enemyList) {
-		if (hitObject == item->enemyNode()) {
-			if (item->hp > 0) {
-				item->Damage();
-			}
-			else {
-			//	imeta->removeTriangleSelector(item->enemyNode.getTriangleSelector());
-				//item->node->remove();
-				item->node->setPosition(core::vector3df(25, 25, 25));
+	for (Opponent* item : opponentList) {
+		if (hitObject == item) {
+		
+			{
+				imeta->removeTriangleSelector(item->getTriangleSelector());
+				item->item->remove();
 			}
 		};
 	}
@@ -60,33 +38,6 @@ EnemyManager::~EnemyManager()
 }
 
 
-Enemy::Enemy() {
-}
 
-void Enemy::Init(scene::ISceneManager* smgr, scene::ITriangleSelector* selector, scene::IMetaTriangleSelector* meta, video::IVideoDriver* driver) {
-	
-	scene::IMesh* barrelMesh = smgr->getMesh("meshes/Barrel.obj");
-	node = smgr->addMeshSceneNode(barrelMesh, 0, 17);
-	if (node)
-	{
-		node->setMaterialFlag(video::EMF_LIGHTING, false);
-		node->setMaterialTexture(0, driver->getTexture("textures/editor_defaults/default_texture.png"));
-		node->setPosition(core::vector3df(19,19,19));
-		selector = smgr->createTriangleSelector(node->getMesh(), node);
-		node->setTriangleSelector(selector);
-		meta->addTriangleSelector(selector);
-		selector->drop();
-		meta->drop();
-	}
-}
-
-scene::IMeshSceneNode* Enemy::enemyNode() {
-	return node;
-}
-
-void Enemy::Damage() {
-	if (hp > 0)
-		hp -= 1;
-}
 
 
--- WestWorld/WestWorld/EnemyManager.h
+++ WestWorld/WestWorld/EnemyManager.h
@@ -5,24 +5,12 @@
 #include <irrlicht.h>
 #include <list>
 #include <vector>
+#include "Opponent.h" 
 //#include <vector>
 
 using namespace std;
 using namespace irr;
 
-class Enemy {
-public:
-	Enemy();
-	~Enemy();
-	void Init(scene::ISceneManager* smgr, scene::ITriangleSelector* selector, scene::IMetaTriangleSelector* meta, video::IVideoDriver* driver);
-	void Damage();
-	int hp = 3;
-	scene::IMeshSceneNode * node;
-	scene::IMeshSceneNode * enemyNode(); // returns the node that belongs to a certain enemy
-	scene::ISceneManager* ismgr;
-	scene::ITriangleSelector* iselector;
-	scene::IMetaTriangleSelector* imeta;
-};
 
 class EnemyManager 
 {
@@ -33,11 +21,14 @@ public:
 	video::IVideoDriver* idriver;
 
 	EnemyManager(scene::ISceneManager* smgr, scene::ITriangleSelector* selector, scene::IMetaTriangleSelector* meta, video::IVideoDriver* driver);
-	void CreateEnemy();
 	void CheckCollision(scene::ISceneNode* collidedObject);
-	void FillList(Enemy* enemy);
+	void FillList(Opponent* enemy);
 	~EnemyManager();
-	std::vector<Enemy*> enemyList;
+
+	std::vector<Opponent*> opponentList;
+
+
+	
 };
 #endif
 
