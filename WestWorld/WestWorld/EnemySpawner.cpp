be9bb6ce7a6d5dbc2ac5c7004558f7baba6d3da6
Daniël <dsrooth@gmail.com>
2018-06-04 15:48:36 +0200

    [ADDS] new Enemy and Revolver models
    Nagant_Revolver uploaded but not implemented

--- WestWorld/WestWorld/EnemySpawner.cpp
+++ WestWorld/WestWorld/EnemySpawner.cpp
@@ -429,7 +429,7 @@ namespace irr
 		}
 
 		void EnemySpawner::SpawnOpponent() {
-			Opponent* spawnPoint = new Opponent(smgr->getMesh("meshes/tempEnemyBlobbert.obj"), smgr->getRootSceneNode(), smgr,-2, smgr->getSceneNodeFromName("Ground"), path->currentPath, this->getAbsolutePosition(), core::vector3df(0, 0, 0), core::vector3df(1.0f, 1.0f, 1.0f), goalNode,_pEnemyManager);
+			Opponent* spawnPoint = new Opponent(smgr->getMesh("meshes/EnemyMesh.obj"), smgr->getRootSceneNode(), smgr,-2, smgr->getSceneNodeFromName("Ground"), path->currentPath, this->getAbsolutePosition(), core::vector3df(0, 0, 0), core::vector3df(1.0f, 1.0f, 1.0f), goalNode,_pEnemyManager);
 			
 		//	spawnPoint->setMaterialFlag(video::EMF_LIGHTING, false);
 		//	spawnPoint->setMaterialTexture(0, driver->getTexture("textures/editor_defaults/default_texture.png"));
